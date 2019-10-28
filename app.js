8e4c7457bb2f8a82f4f400cdfbf9f81da9383dbc
Anonymous <home@Anonymouss-MacBook-Pro.local>
2019-10-28 16:50:10 -0400

    initial commit

--- app.js
+++ app.js
@@ -2,23 +2,56 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+
 
 var app = express();
 
-app.use(bodyParser({extended:true}));
+app.use(express.static('public'))
+
+let items = ['One', 'Two', 'Three']
+let workList = []
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
 
-  var today = new Date();
-  var currentDate = today.getDay();
-  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let day = date.getDate()
 
-  day = days[currentDate]
-
-  res.render("list", {kindOfDay: day});
+  res.render("list", {listTitle: day, newAddItem:items});
+  console.log(items)
 
 });
+
+app.get('/work', function(req,res){
+  res.render('list', {listTitle: "work", newAddItem:workList})
+})
+
+app.get('/about', function(req,res){
+  res.render('about')
+})
+
+
+app.post('/', function(req,res){
+  console.log(req.body)
+  if (req.body.list === "work"){
+    item = req.body.newItem;
+    workList.push(item);
+    res.redirect('/work');
+  }else{
+    item = req.body.newItem
+    items.push(item)
+  res.redirect('/')
+  }
+
+})
+
+app.post('/work', function(req,res){
+  item = req.body.newItem
+  items.push(item)
+  res.redirect('/work')
+})
 
 
 app.listen(3000, function(){
